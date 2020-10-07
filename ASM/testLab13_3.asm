80626b7b1a1bb6b173865256eb74e9a7a43a2848
Nolva <1090034020@qq.com>
2020-10-07 21:33:10 +0800

    学习jack语言 & ASM外中断

---
+++ ASM/testLab13_3.asm
@@ -0,0 +1,29 @@
+assume cs:code
+data segment
+    db 'conversation',0
+data ends
+code segment
+start:
+    mov ax,data
+    mov ds,ax
+    mov si,0
+    mov ax,0B800H
+    mov es,ax
+    mov di,13*160
+    mov bx,offset flag-offset flagend
+flag:
+    cmp byte ptr ds:[si],0
+    je ok
+    mov al,ds:[si]
+    mov byte ptr es:[di],al
+    mov byte ptr es:[di+1],2
+    inc si
+    add di,2
+    int 7CH
+flagend:
+    nop
+ok:
+    mov ax,4C00H
+    int 21H
+code ends
+end start
